5b7c1a6b87644db7b244a12465dd1861172808b9
tuan <adgtuan@gmail.com>
2019-03-10 23:10:14 +0700

    third

--- Source.cpp
+++ Source.cpp
@@ -11,13 +11,6 @@ struct QInt {
 	unsigned int data[4] = {0};
 };
 
-//char String_Mod_2(string str)
-//{
-//	int so = stoi(str);
-//	int ketqua = so % 2;
-//
-//}
-
 
 char Int_Sang_Char(int x)
 {
@@ -112,7 +105,7 @@ void String_Chia_2(string str,string &phanDu,string &phanNguyen)
 				phanNguyen.push_back(songuyen);
 				soBiChia_Str.clear();
 				i++;
-				if (soDu == 0&& str[i] != '1'&& str[i] != '0')
+				if (soDu == 0&& str[i] != '1' && str[i] != '0')
 				{
 					soBiChia_Str.push_back(str[i]);
 				}
@@ -147,7 +140,6 @@ void String_Chia_2(string str,string &phanDu,string &phanNguyen)
 				soBiChia_Int = stoi(soBiChia_Str);
 			}
 		}
-		
 	}
 }
 
@@ -226,6 +218,7 @@ void Doi_Sang_Nhi_Phan(string str,int ketQua[])
 	while (1)
 	{
 		String_Chia_2(str, PhanDu, PhanNguyen);
+		// điều kiện dừng là khi phần nguyên bằng 0 
 		if (PhanNguyen == "0")
 		{
 			temp[i] = stoi(PhanDu);
@@ -240,18 +233,23 @@ void Doi_Sang_Nhi_Phan(string str,int ketQua[])
 			i++;
 		}
 	}
+
+	//đảo chiều dãy số nhị phân để đúng thứ tự
 	int j = 0;
 	for (int i = MAX-1; i >= 0; i--)
 	{
 		ketQua[j] = temp[i];
 		j++;
 	}
-	
+
+	// nếu là số âm thì lấy giá trị bù 2
 	if (flag == true)
 	{
 		LaySoBu2(ketQua);
 	}
 }
+
+// hàm scanf Qint
 void ScanfQInt(QInt &number, int dayNhiPhan[])
 {
 	for (int i = 0; i < MAX; i++)
@@ -262,8 +260,82 @@ void ScanfQInt(QInt &number, int dayNhiPhan[])
 		}
 	}
 	cout << endl;
-	cout <<bitset<32>(number.data[3]) << endl;  //in ra dãy bit
+	cout << bitset<32>(number.data[0]) << bitset<32>(number.data[1]) << bitset<32>(number.data[2]) << bitset<32>(number.data[3]) << endl;  //in ra dãy bit
 }
+
+string String_Nhan_2(string str)
+{
+	string ketQua;
+	int n = str.size();
+	int soDu=0;
+	string ch; //để lưu giá trị số nhân
+	int x;//để lưu giá trị số nhân ở dạng số nguyên
+	int result;// để lưu kết quả phép nhân
+	char temp;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		ch = str[i];
+		x = stoi(ch);
+		result = x * 2;
+		if (result < 10)
+		{
+			if (soDu == 1)
+			{
+				result += 1;
+				soDu = 0;
+			}
+			temp=Int_Sang_Char(result);
+			ketQua.push_back(temp);
+		}
+		else
+		{
+			result = result % 10;
+			if (soDu == 1)
+			{
+				result += 1;
+			}
+			soDu = 1;
+			temp = Int_Sang_Char(result);
+			ketQua.push_back(temp);
+		}
+	}
+	if (soDu == 1)
+	{
+		ketQua.push_back('1');
+	}
+	n = ketQua.size();
+	for (int i = 0; i < n / 2; i++)
+	{
+		swap(ketQua[i], ketQua[n - 1 - i]);
+	}
+	return ketQua;
+}
+
+string _2_Mu_N(int n)
+{
+	string ketQua="1";
+	for (int i = 1; i <= n; i++)
+	{
+		ketQua = String_Nhan_2(ketQua);
+	}
+	return ketQua;
+}
+
+// hàm xuất số QInt 
+void PrintQInt(QInt number)
+{
+	int a[MAX] = {0};
+	for (int i = 0; i < MAX; i++)
+	{
+		if ((number.data[i/32] >> (32 - 1 - i%32)) & 1 == 1)
+		{
+			a[i] = 1;
+		}
+	}
+	
+}
+
+
 int main()
 {
 	QInt number;
@@ -271,12 +343,17 @@ int main()
 	string a, b;
 	int dayNhiPhan[MAX];
 	Doi_Sang_Nhi_Phan(s,dayNhiPhan);
-	ScanfQInt(number, dayNhiPhan);*/
-	string s = "-8793278316383117319";
+	ScanfQInt(number, dayNhiPhan);
+	string s = "-9223372036854775807";
 	string a, b;
 	int dayNhiPhan[MAX];
 	Doi_Sang_Nhi_Phan(s, dayNhiPhan);
 	ScanfQInt(number, dayNhiPhan);
+	PrintQInt(number);
+	*/
+	string s = "5";
+	string kq = _2_Mu_N(139);
+	cout << kq;
 	system("pause");
 	return 0;
 }
